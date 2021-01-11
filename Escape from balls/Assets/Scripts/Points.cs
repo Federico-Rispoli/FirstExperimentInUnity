8f30d99d9e9c63d36a955a16476310dcf8ac4f93
Federico Rispoli <federicorispoli96@gmail.com>
2021-01-11 18:27:21 +0100

    Add Timer in Game scene and Death scene

---
+++ Escape from balls/Assets/Scripts/Points.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Runtime;
+
+public class Points : MonoBehaviour
+{
+    public Text punteggio;
+    public static float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Game")
+        {
+            timer = 0;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Game") {
+            timer += Time.deltaTime;
+            punteggio.text = "Time: " + timer;
+        }
+        else if (SceneManager.GetActiveScene().name == "Death")
+        {
+            punteggio.text = "Time: " + Mathf.Round(timer * 1000.0f) * 0.001f;
+        }
+    }
+}
